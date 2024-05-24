b17402d0433e6aba715074c40d026b4450302e67
Peter McGibney <peter@mapandsnap.org>
2024-05-24 13:32:44 -0400

    feat: add Category model

---
+++ Bulk/BulkNess12/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkNess12.Models
+{
+    public class Category
+    {
+        // Columns in the table.
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
