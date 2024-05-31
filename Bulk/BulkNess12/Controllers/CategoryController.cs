b303d35a98c73ea10c8dc3ce51a24c5cc6fcb8a0
Peter McGibney <peter.mcgibney@gmail.com>
2024-05-31 17:21:43 -0400

    feat: add data retrieval for categories

--- Bulk/BulkNess12/Controllers/CategoryController.cs
+++ Bulk/BulkNess12/Controllers/CategoryController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkNess12.Data;
+using BulkNess12.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkNess12.Controllers
 {
     public class CategoryController : Controller
     {
+       
+        private readonly ApplicationDbContext _db;
+        // Implementation of db context
+        public CategoryController(ApplicationDbContext db)
+        // Assign the db context to the local variable
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
+            // Retrieve all the categories as a list and store it in objCategoryList
+            List<Category> objCategoryList = _db.Categories.ToList(); // <-- this is typed
             return View();
         }
     }
