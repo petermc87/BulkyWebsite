5fb0e9904f7b0d58411df21d07cf007302e2f17a
Peter McGibney <peter.mcgibney@gmail.com>
2024-06-10 11:14:27 -0400

    feat: add input field model links and text display

--- Bulk/BulkNess12/Models/Category.cs
+++ Bulk/BulkNess12/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkNess12.Models
 {
@@ -8,7 +9,9 @@ namespace BulkNess12.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")] //<-- This is the text that is passed into the input on the front end.
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
