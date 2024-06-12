9516c3fe3d4d9918b68f4b0b8bebae9652635916
Peter McGibney <peter.mcgibney@gmail.com>
2024-06-11 21:49:09 -0400

    feat: Add create method, presist to database and tag helpers for error messages

--- Bulk/BulkNess12/Controllers/CategoryController.cs
+++ Bulk/BulkNess12/Controllers/CategoryController.cs
@@ -8,7 +8,7 @@ namespace BulkNess12.Controllers
 {
     public class CategoryController : Controller
     {
-       
+
         private readonly ApplicationDbContext _db;
         // Implementation of db context
         public CategoryController(ApplicationDbContext db)
@@ -26,6 +26,17 @@ namespace BulkNess12.Controllers
 
         public IActionResult Create()
         {
+            return View();
+        }
+        [HttpPost] // <-- Http request for form posted in the Create.cshtml
+        public IActionResult Create(Category obj)
+        {
+            if(ModelState.IsValid) // If the validation from the model is correct, then persist to db.
+            {
+                _db.Categories.Add(obj); //<-- Pointing to the obj to be saved to the db
+                _db.SaveChanges();//<-- Saving to db.
+                return RedirectToAction("Index");
+            }
             return View();
         }
     }
--- Bulk/BulkNess12/Models/Category.cs
+++ Bulk/BulkNess12/Models/Category.cs
@@ -9,9 +9,11 @@ namespace BulkNess12.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(30)] // <-- validation state that checks if its less than or equal to 30 chars.
         [DisplayName("Category Name")] //<-- This is the text that is passed into the input on the front end.
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1 and 100")]
         public int DisplayOrder { get; set; }
     }
 }
