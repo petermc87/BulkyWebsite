6f850c8ece93fb712f61901be197c49c0886a7c8
Peter McGibney <peter.mcgibney@gmail.com>
2024-06-24 21:53:21 -0400

    feat: add categories page and test with placeholder

---
+++ Bulk/BulktWebrazor_temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulktWebrazor_temp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
