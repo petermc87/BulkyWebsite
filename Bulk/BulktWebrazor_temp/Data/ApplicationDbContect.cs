5335c7c300679cab26d0a0a68ea7f4ad5b9ec9cd
Peter McGibney <peter.mcgibney@gmail.com>
2024-06-24 21:06:26 -0400

    feat: setup razor project and database

---
+++ Bulk/BulktWebrazor_temp/Data/ApplicationDbContect.cs
@@ -0,0 +1,23 @@
+﻿using BulktWebrazor_temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulktWebrazor_temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Thriller", DisplayOrder = 4 },
+                new Category { Id = 3, Name = "Horror", DisplayOrder = 3 }
+                );
+        }
+    }
+}
