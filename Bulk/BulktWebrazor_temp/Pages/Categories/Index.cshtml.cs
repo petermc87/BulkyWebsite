bc00719d4d92e9234c0800048a0da9f06b2a59b5
Peter McGibney <peter.mcgibney@gmail.com>
2024-06-25 23:38:49 -0400

    feat: add category page

--- Bulk/BulktWebrazor_temp/Pages/Categories/Index.cshtml.cs
+++ Bulk/BulktWebrazor_temp/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,27 @@
+using BulktWebrazor_temp.Data;
+using BulktWebrazor_temp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulktWebrazor_temp.Pages.Categories
 {
+    // Constructor
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _db;
+        // Declare categoryList variable
+        public List<Category> CategoryList { get; set; } //--> "This property is a publicly accessible list of Category objects and is named CategoryList."
+        // Dependency Injection
+        public IndexModel(ApplicationDbContext db)
         {
+            _db = db;
         }
+       
+        public void OnGet() // NOTE: Void is used here because there is no return value.
+        {
+            CategoryList = _db.Categories.ToList();
+        }
+
     }
+
 }
