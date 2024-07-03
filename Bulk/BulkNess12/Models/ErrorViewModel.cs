5c8ab9fa7c8f39111bc06283ea0270de9e69de8d
Peter McGibney <peter.mcgibney@gmail.com>
2024-07-03 09:53:23 -0400

    chore: move folders from BulkNess12 to the newer projects

--- Bulk/BulkNess12/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BulkNess12.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
