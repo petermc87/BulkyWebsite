18fdee78b27287efdda39fc944906e9952f3d01a
Peter McGibney <peter.mcgibney@gmail.com>
2024-07-07 23:22:15 -0400

    feat: add respository pattern folder structure

--- BulkyWeb.DataAccess/Repository/IRepository/IRepository.cs
+++ BulkyWeb.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,12 +7,17 @@ using System.Threading.Tasks;
 
 namespace BulkyWeb.DataAccess.Repository.IRepository
 {
+    // Generic Repository Pattern
+    // NOTE: Update is that used in Generic because of how nuanced it is.
     internal interface IRepository<T> where T : class 
     {
         // T - Category
         IEnumerable<T> GetAll();
         // A representation of the link operation shown in the MVC controller: Category? foundCategory1 = _db.Categories.FirstOrDefault(u => u.Id == id);
         T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void RemoveRange(IEnumerable<T> entity); // <-- Removes all objects
+        
 
     }
 }
