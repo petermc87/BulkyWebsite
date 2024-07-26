67f57c5f6fdc2e265fab97bbd22a9d5baed49119
Peter McGibney <peter.mcgibney@gmail.com>
2024-07-25 22:14:11 -0400

    feat: create product controller

---
+++ Bulk/BulkNess12/Areas/Admin/Controllers/Class.cs
@@ -0,0 +1,42 @@
+﻿using Bulky.Models;
+using BulkyWeb.DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkNess12.Areas.Admin.Controllers
+{
+    public class ProductController : Product
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        //--- READ ---//
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+
+            //TODO: Add view here
+            // return View(objProductList)
+
+        }
+
+        //TODO:
+        
+        //--- CREATE ---//
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Product obj)
+            if (object.)
+    }
+}
+
+
+
