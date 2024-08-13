5143cdcba3a25b76361e12a7b314dbfc35f7f369
Peter McGibney <peter.mcgibney@gmail.com>
2024-08-12 22:52:47 -0400

    refactor: change from Viewbag method to View model

---
+++ Bulky.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.Models.ViewModels
+{
+    internal class ProductVM
+    {
+        public int Product { get; set; }
+        // Dropdown
+        public IEnumerable<SelectListItem> CategoryList { get; set; }
+
+
+    }
+}
