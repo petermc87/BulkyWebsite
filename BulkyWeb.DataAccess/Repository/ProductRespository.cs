b617bf1809a14958d4393ed0076b71c026498b60
Peter McGibney <peter.mcgibney@gmail.com>
2024-08-29 03:08:05 -0400

    refactor: change update method in ProductRepository so it can handle manual mapping

--- BulkyWeb.DataAccess/Repository/ProductRespository.cs
+++ BulkyWeb.DataAccess/Repository/ProductRespository.cs
@@ -19,7 +19,28 @@ namespace BulkyWeb.DataAccess.Repository
 
         public void Update(Product obj)
         {
-            _db.Products.Update(obj);
+            // Retrieve the object to be updated from the db
+            var objFromDb = _db.Products.FirstOrDefault(x => x.Id == obj.Id);
+            
+            // Manual mapping of each element of the product model.
+            //-- This is why Update belongs here and not in the generic repository --//
+            if (objFromDb != null)
+            {
+                objFromDb.Title = obj.Title;
+                objFromDb.Description = obj.Description;
+                objFromDb.Price = obj.Price;
+                objFromDb.ListPrice = obj.ListPrice;
+                objFromDb.Price50 = obj.Price50;
+                objFromDb.Price100 = obj.Price100;
+                objFromDb.ISBN = obj.ISBN;
+                objFromDb.Author = obj.Author;
+                objFromDb.CategoryId = obj.CategoryId;
+                if(obj.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = obj.ImageUrl;
+                }
+            }
+            
         }
     }
 }
