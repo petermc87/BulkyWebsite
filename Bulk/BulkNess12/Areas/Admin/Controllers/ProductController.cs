66a9d4eabfacae8e3807db75ac5f3f8cddc5f7e5
Peter McGibney <peter.mcgibney@gmail.com>
2024-09-12 22:43:24 -0400

    feat: add datatables.net api

--- Bulk/BulkNess12/Areas/Admin/Controllers/ProductController.cs
+++ Bulk/BulkNess12/Areas/Admin/Controllers/ProductController.cs
@@ -21,7 +21,8 @@ namespace BulkNess12.Areas.Admin.Controllers
         //--- READ ---//
         public IActionResult Index()
         {
-            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            // Adding the Category property to the Product by using the includeProperties method.
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
             return View(objProductList);
         }
 
@@ -155,159 +156,19 @@ namespace BulkNess12.Areas.Admin.Controllers
             TempData["success"] = "Successfully deleted product";
             return RedirectToAction("index");
         }
+
+
+        // Using datatables.net to retrieve data from an API
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // Adding the Category property to the Product by using the includeProperties method.
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+        }
+
+        #endregion
     }
 }
-
-
-//using Bulky.Models;
-//using BulkyWeb.DataAccess.Repository;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.AspNetCore.Mvc.Rendering;
-//using Bulky.Models.ViewModels;
-
-//namespace BulkNess12.Areas.Admin.Controllers
-//{
-//    public class ProductController : Controller
-//    {
-//        private readonly IUnitOfWork _unitOfWork;
-
-//        //public object ModelState { get; private set; }
-
-//        public ProductController(IUnitOfWork unitOfWork)
-//        {
-//            _unitOfWork = unitOfWork;
-//        }
-
-//        //--- READ ---//
-//        public IActionResult Index()
-//        {
-//            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
-
-//            return View(objProductList);
-
-//        }
-
-//        //--- UPSERT ---//
-//        public IActionResult Upsert(int? id)
-//        {
-//            // Creating a new ProductVM instance.
-//            ProductVM productVM = new()
-//            {
-//                //  Generating a new list item to be added to the CategoryList. This generates the list of
-//                // categories in a dropdown for selection
-//                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-//                {
-//                    // Populating the dropdown with this text
-//                    Text = u.Name,
-//                    Value = u.Id.ToString()
-//                }),
-//                Product = new Product()
-//            };
-//            if (id == null || id == 0)
-//            {
-//                // Create product -> the "View" part is linked to the Upsert.cshtml for processing.
-//                return View(productVM);
-//            }
-//            else
-//            {
-//                // Update product.
-//                productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
-//                return View(productVM);
-//            }
-
-//        }
-//        [HttpPost]
-//        // Add in the form file, which is related to the image being uploaded in the form.
-//        public IActionResult Create(ProductVM productVM, IFormFile? file)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                _unitOfWork.Product.Add(productVM.Product);
-//                _unitOfWork.Save();
-//                TempData["success"] = "Product created successfully";
-//                return RedirectToAction("Index");
-//            }
-//            else
-//            {
-//                // Populate cat list dropdown (genre) in order to avoid the exception error.
-//                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
-//                {
-//                    Text = u.Name,
-//                    Value = u.Id.ToString()
-//                });
-//                return View(productVM);
-//            }
-//        }
-
-
-//        //--- UPDATE ---//
-//        public IActionResult Edit(int? id)
-//        {
-//            if (id == 0 || id == null)
-//            {
-//                return NotFound();
-//            }
-
-//            // Finding the product by the id.
-//            Product? foundProduct = _unitOfWork.Product.Get(u => u.Id == id);
-
-//            if (foundProduct == null)
-//            {
-//                return NotFound();
-//            }
-
-//            //Passing the object to the view if found
-//            return View(foundProduct);
-//        }
-
-//        // Updating the db
-
-//        [HttpPost]
-//        public IActionResult Edit(Product Obj)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                _unitOfWork.Product.Update(Obj);
-//                _unitOfWork.Save();
-//                TempData["success"] = "Product Updated";
-//                return RedirectToAction("Index");
-//            }
-//            return View();
-//        }
-
-//        public IActionResult Delete(int? id)
-//        {
-//            if (id == 0 || id == null)
-//            {
-//                return NotFound();
-//            }
-
-//            Product? returnedProduct = _unitOfWork.Product.Get(u => u.Id == id);
-
-//            if (returnedProduct == null)
-//            {
-//                return NotFound();
-//            }
-//            return View(returnedProduct);
-//        }
-//        [HttpPost, ActionName("Delete")]
-
-//        public IActionResult DeletePOST(int? id)
-//        {
-//            Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
-
-//            if (obj == null)
-//            {
-//                return NotFound();
-//            }
-
-//            _unitOfWork.Product.Remove(obj);
-//            _unitOfWork.Save();
-//            TempData["success"] = "Successfully deleted product";
-//            return RedirectToAction("index");
-//        }
-//    }
-//}
-
-
-
---
+++ Bulk/BulkNess12/wwwroot/js/product.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+})
+
+// Match the key name with whats in the json returned from the API.
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/product/getall' },
+        "columns": [
+            // Make sure the 'key' property is exacrtly like it is in the API JSON.
+            { data: 'title', 'width': '15%' },
+            { data: 'isbn', 'width': '15%' },
+            { data: 'listPrice', 'width': '15%' },
+            { data: 'author', 'width': '15%' },
+            // This is category.name because it is nested in product.
+            { data: 'category.name', 'width': '15%' },
+        ]
+    })
+}
