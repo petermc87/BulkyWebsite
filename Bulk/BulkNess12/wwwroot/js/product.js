e6a8045746fe47572e3c1dcc529125fae7aec61c
Peter McGibney <peter.mcgibney@gmail.com>
2024-09-12 23:04:08 -0400

    refactor: update the product.js to edit data

--- Bulk/BulkNess12/wwwroot/js/product.js
+++ Bulk/BulkNess12/wwwroot/js/product.js
@@ -14,6 +14,23 @@ function loadDataTable() {
             { data: 'author', 'width': '15%' },
             // This is category.name because it is nested in product.
             { data: 'category.name', 'width': '15%' },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                                <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"
+                                    <i class="bi bi-pencil-square"></i>
+                                    Edit
+                                </a>
+                                <a href="" class="btn btn-danger mx-2"
+                                    <i class="bi bi-trash-fill"></i>
+                                   Delete
+                                </a>
+                            </div>`
+                }, 
+                'width': '15%'
+            }
         ]
     })
 }
+
