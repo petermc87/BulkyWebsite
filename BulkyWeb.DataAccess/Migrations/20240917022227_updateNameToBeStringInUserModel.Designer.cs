7c6bd1759233124682f5daa38af1e0d7640cef62
Peter McGibney <peter.mcgibney@gmail.com>
2024-09-16 22:26:12 -0400

    refactor: change cconfiguration, styling and Resgister.cshtml.cs

--- BulkyWeb.DataAccess/Migrations/20240917022227_updateNameToBeStringInUserModel.Designer.cs
+++ BulkyWeb.DataAccess/Migrations/20240917022227_updateNameToBeStringInUserModel.Designer.cs
@@ -4,6 +4,7 @@ using BulkyWeb.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BulkyWeb.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240917022227_updateNameToBeStringInUserModel")]
+    partial class updateNameToBeStringInUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
