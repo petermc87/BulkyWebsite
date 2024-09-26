74dccce5cfd8ae3a77cb9607c12b3e5e607203d6
Peter McGibney <peter.mcgibney@gmail.com>
2024-09-25 21:35:22 -0400

    Add shopping cart model and table to db

--- BulkyWeb.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyWeb.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,7 @@ namespace BulkyWeb.DataAccess.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
         //Default entity framework builder for seeding
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
