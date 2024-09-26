c4b31a408c7a6033991cf0c2cb551c909372e5e5
Peter McGibney <peter.mcgibney@gmail.com>
2024-09-25 21:40:43 -0400

    Congifure Repository IRepository and UnitOfWork for ShoppingCart

---
+++ BulkyWeb.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,33 @@
+﻿using Bulky.Models;
+using BulkyWeb.DataAccess.Data;
+using BulkyWeb.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyWeb.DataAccess.Repository
+{
+    // NOTE: If you hit CTRL + . over the ICategoryRepository section it will create
+    // the interface including the Add, Remove, etc which are already defined in
+    // the Repository file.
+    public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+    {
+        // Declaring the db context here again
+        private ApplicationDbContext _db;
+        // Inject the db context dependency to the CategoryRepository.
+        // This then is passed to the base class i.e. Repository<Category>
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+  
+
+        public void Update(ShoppingCart obj)
+        {
+            _db.ShoppingCarts.Update(obj);
+        }
+    }
+}
