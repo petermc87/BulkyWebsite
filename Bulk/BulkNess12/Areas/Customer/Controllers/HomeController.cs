6aa5422f35f5aa32f05316277064280849749217
Peter McGibney <peter.mcgibney@gmail.com>
2024-09-25 22:24:56 -0400

    refactor: change the model in Details.cshml, where Product model can be access as a foreign key and then add details actions for shoppingcart

--- Bulk/BulkNess12/Areas/Customer/Controllers/HomeController.cs
+++ Bulk/BulkNess12/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Bulky.Models;
 using BulkyWeb.DataAccess.Repository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BulkNess12.Areas.Customer.Controllers
 {
@@ -26,8 +28,28 @@ namespace BulkNess12.Areas.Customer.Controllers
 
         public IActionResult Details(int productId)
         {
-            Product product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
-            return View(product);
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+                Count = 1,
+                ProductId = productId,
+            };
+            
+            return View(cart);
+        }
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            _unitOfWork.Save();
+
+
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
