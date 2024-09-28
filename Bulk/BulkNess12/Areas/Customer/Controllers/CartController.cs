37a3b408ff4723411fb7146ff25414894dc24d31
Peter McGibney <peter.mcgibney@gmail.com>
2024-09-27 21:40:10 -0400

    Add CartController, associated index view, update repository to include filter for category and view model for showing order total and cart list

---
+++ Bulk/BulkNess12/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using BulkyWeb.DataAccess.Repository;
+using Bulky.Models.ViewModels;
+using System.Security.Claims;
+
+namespace BulkNess12.Areas.Customer.Controllers
+{
+    [Area("customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Properties")
+            };
+
+            return View(ShoppingCartVM);
+        }
+    }
+}
