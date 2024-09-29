86f1928b2b92b98c7d7295edbe72a10fab2e224a
Peter McGibney <peter.mcgibney@gmail.com>
2024-09-29 02:38:23 -0400

    feat: Add cart calculation, live cart update and UI implemenation

--- Bulk/BulkNess12/Areas/Customer/Controllers/CartController.cs
+++ Bulk/BulkNess12/Areas/Customer/Controllers/CartController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using BulkyWeb.DataAccess.Repository;
 using Bulky.Models.ViewModels;
 using System.Security.Claims;
-
+using Bulky.Models;
 namespace BulkNess12.Areas.Customer.Controllers
 {
     [Area("customer")]
@@ -19,15 +19,81 @@ namespace BulkNess12.Areas.Customer.Controllers
         }
         public IActionResult Index()
         {
+            // Grabbing the userid.
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
             ShoppingCartVM = new()
             {
-                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Properties")
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product")
             };
+            // Indexing the cart in the VM list and adding the cart total based on the pricing parameters
+            foreach(var cart in ShoppingCartVM.ShoppingCartList)
+            {
+               cart.Price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+
+            }
 
             return View(ShoppingCartVM);
+        }
+
+
+        // Cart update action methods.
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            // Redirecting to the shopping cart index, not the Home index!!
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+           // If item is 1, the we remove from cart
+           if(cartFromDb.Count <= 1) {
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            } else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+        {
+            // Returning the price based on quantity (the more they order the lower the unit price)
+            if(shoppingCart.Count <= 50)
+            {
+                return shoppingCart.Product.Price;
+            } else
+            {
+                if (shoppingCart.Count <= 100)
+                {
+                    return shoppingCart.Product.Price50;
+                } else
+                {
+                    return shoppingCart.Product.Price100;
+                }
+            }
+
         }
     }
 }
