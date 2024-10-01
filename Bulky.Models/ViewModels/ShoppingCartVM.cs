14efa69d6115244c23a18184c036be240cf35dc2
Peter McGibney <peter.mcgibney@gmail.com>
2024-09-30 21:17:00 -0400

    refactor: change shopping cart VM to include header details model and the OrderTotal references in CartController and Index.cshtml

--- Bulky.Models/ViewModels/ShoppingCartVM.cs
+++ Bulky.Models/ViewModels/ShoppingCartVM.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// NOTE: a view model is where the required info for a view is defined.
 namespace Bulky.Models.ViewModels
 {
     public class ShoppingCartVM
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
-        public double OrderTotal { get; set; }
+       
+        public OrderHeader OrderHeader { get; set; }
+
 
     }
 }
