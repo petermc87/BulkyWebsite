15d329a010813514c61171fc703b69113e1571d5
Peter McGibney <peter.mcgibney@gmail.com>
2024-10-05 22:56:07 -0400

    Create OrderController, a new index view, add navigation link in the _Layout, datatables files and VM containing only the required information

---
+++ Bulk/BulkNess12/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿using BulkyWeb.DataAccess.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Bulky.Models;
+
+namespace BulkNess12.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+
+		// Generating a class and passing in a variable called unitOfwork with a type IUnitOfWork
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			// Assignment.
+			_unitOfWork = unitOfWork;
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		// Using datatables.net to retrieve data from an API
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			// Retrieving the orderheader list.
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = objOrderHeaders});
+		}
+
+
+		#endregion
+
+	}
+}
