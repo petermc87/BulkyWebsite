45896aeb5c45dde82aa4e3458fc7320fd51a14f6
Peter McGibney <peter.mcgibney@gmail.com>
2024-10-31 21:30:12 -0400

    Create user account through admin portal

--- Bulk/BulkNess12/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Bulk/BulkNess12/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -214,7 +214,15 @@ namespace BulkNess12.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created Successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
