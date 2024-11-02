b233f8b6c008833b0962c17949d14f40f8de8dc3
Peter McGibney <peter.mcgibney@gmail.com>
2024-11-01 21:46:32 -0400

    Create db initializer

--- Bulk/BulkNess12/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Bulk/BulkNess12/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -129,16 +129,7 @@ namespace BulkNess12.Areas.Identity.Pages.Account
 
         public async Task OnGetAsync(string returnUrl = null)
         {
-            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
-            {
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
 
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
-
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
-
-            }
 
             Input = new()
             {
